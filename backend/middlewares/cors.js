2942a33a481f5ef420955e6776b06f8efb2bdfcf
Anton Sarkisyan <slatrex@yandex.ru>
2021-04-25 18:42:23 +0300

    hotfix: исправить ошибки

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,8 +1,20 @@
 const cors = require('cors');
 
+const whiteList = [
+  'http://mesto.anton-sarkisyan.nomoredomains.club',
+  'https://mesto.anton-sarkisyan.nomoredomains.club'
+]
+
 const corsOptions = {
-  origin: ['http://localhost:3001', 'http://mesto.anton-sarkisyan.nomoredomains.club', 'https://mesto.anton-sarkisyan.nomoredomains.club'],
-  optionsSuccessStatus: 200,
+  origin(origin, callback) {
+    if(whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by Cors'));
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
   credentials: true,
 };
 
